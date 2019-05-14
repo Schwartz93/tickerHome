8f45b2377cd17ad799a9bf84562230e89f06f829
michael.schwartz <michael.schwartz93@hotmail.com>
2019-05-14 12:49:30 +0200

    Missing: refresh && title undefined.

---
+++ ticker-firma/assets/scripts/script.js
@@ -0,0 +1,100 @@
+const output = {
+    "messages": [
+        {
+            "id": "1",
+            "title": "Wechsel bei Rapid",
+            "text": "Letzter wechsel Rapid. Sax geht aus dem Spiel, neu aufs Feld kommt Demaku.",
+            "time": "23'"
+        },
+        {
+            "id": "2",
+            "title": "Gelbe Karte gegen Rapid und Austria",
+            "text": "Nach einer Austria-Flanke liegt Stebinger am Boden.",
+            "time": "35'"
+        },
+        {
+            "id": "3",
+            "title": "Wechsel bei Austria",
+            "text": "Letzter wechsel Rapid. Sax geht aus dem Spiel, neu aufs Feld kommt Demaku.",
+            "time": "40'"
+        },
+        {
+            "id": "4",
+            "title": "Rot nach schwerem Foul",
+            "text": "F. Klein mit Rettung abtransportiert.",
+            "time": "42'"
+        },
+        {
+            "id": "5",
+            "title": "Tor für Rapid",
+            "text": "Tor in der 50. Minute nach einer Ecke von Schwab.",
+            "time": "49'"
+        },
+        {
+            "id": "6",
+            "title": "Tor für die Austria",
+            "text": "Tor in der 50. Minute nach einer Ecke von Fitz.",
+            "time": "54'"
+        },
+    ]
+};
+
+(function show3Messages() {
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    document.querySelector(".badge").innerHTML += output.messages.length;
+    document.querySelector(".container").innerHTML += "<div class='tickerMessages'></div>";
+    document.querySelector(".updated_at").innerHTML += time;
+
+    let display = '';
+    display += "<button class='show btn btn-warning' onclick='showAll();'>Show all</button>";
+
+    for (let i = 0; i < output.messages.length; i++) {
+        if (i < 3) {
+            document.querySelector(".tickerMessages").innerHTML = display;
+            display += "<div class='singleMessage'>"
+                + "<h3>" + output.messages[i]["title"] + "</h3>"
+                + "<p>" + output.messages[i]["text"] + "</p>"
+                + "<span>" + output.messages[i]["time"] + "<span>"
+                + "</div>";
+        } else {
+            document.querySelector(".tickerMessages").innerHTML = display;
+            display += "<div class='hidden singleMessage'>"
+                + "<h3 class='hidden'>" + output.messages[i]["title"] + "</h3>"
+                + "<p class='hidden'>" + output.messages[i]["text"] + "</p>"
+                + "<span class='hidden'>" + output.messages[i]["time"] + "<span>"
+                + "</div>";
+        }
+    }
+}());
+
+function showAll() {
+    let display = '';
+    display += "<button class=\"show btn btn-warning\" onclick=\"reverseOrder(); \">Reverse Order</button>";
+
+    for (let i = 0; i <= output.messages.length; i++) {
+        document.querySelector(".tickerMessages").innerHTML = display;
+        display += "<div class='singleMessage'>"
+            + "<h3>" + output.messages[i]["title"] + "</h3>"
+            + "<p>" + output.messages[i]["text"] + "</p>"
+            + "<span>" + output.messages[i]["time"] + "<span>"
+            + "</div>";
+    }
+}
+
+function reverseOrder() {
+    let display = '';
+    output.messages.reverse();
+    display += "<button class=\"show btn btn-warning\" onclick=\"reverseOrder(); \">Reverse Order</button>";
+
+    for (let i = 0; i <= output.messages.length; i++) {
+        document.querySelector(".tickerMessages").innerHTML = display;
+        display += "<div class='singleMessage'>"
+            + "<h3>" + output.messages[i]["title"] + "</h3>"
+            + "<p>" + output.messages[i]["text"] + "</p>"
+            + "<span>" + output.messages[i]["time"] + "<span>"
+            + "</div>";
+    }
+
+}
---
+++ ticker-firma/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <title>Ticker JS</title>
+</head>
+<body>
+<nav class="navbar navbar-dark bg-dark">
+    <button type="button" class="test btn btn-warning">
+        Total Notifications: <span class="badge badge-light"></span>
+    </button>
+    <h2>Live Ticker</h2>
+    <span class="updated_at">Last automated update: </span>
+</nav>
+
+<div class="container">
+
+</div>
+<!--<button class="show btn btn-warning" onclick="showAll(); this.onclick=null;">Show more</button>-->
+<!--<button class="show btn btn-warning" onclick="reverseOrder(); ">Reverse Order</button>-->
+<script src="assets/scripts/script.js"></script>
+</body>
+</html>
